e66276b2745d83e697f00fba382ae5ead972fb7f
AwZatarra <poolriveramolina@gmail.com>
2021-09-06 18:49:38 -0500

    correccion2

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,7 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('product.id'));
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
     this.product = products.find(product => product.id === productIdFromRoute);
   }
